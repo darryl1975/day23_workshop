6f796c32f5b32a41eca9ac2ddcfd9d427007c0c7
Darryl Ng <darryl1975@hotmail.com>
2023-03-14 16:05:21 +0800

    workshop day 23 completed

--- src/main/java/sg/edu/nus/iss/day23_workshop/controller/OrderController.java
+++ src/main/java/sg/edu/nus/iss/day23_workshop/controller/OrderController.java
@@ -1,12 +1,17 @@
 package sg.edu.nus.iss.day23_workshop.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.MergedAnnotations.Search;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import sg.edu.nus.iss.day23_workshop.model.Order;
 import sg.edu.nus.iss.day23_workshop.payload.SearchRequest;
 import sg.edu.nus.iss.day23_workshop.service.OrderRestTemplateService;
 
@@ -23,5 +28,13 @@ public class OrderController {
         model.addAttribute("searchObject", sr);
 
         return "searchorder";
+    }
+
+    @PostMapping("/search") 
+    public String processSearch(@ModelAttribute("searchObject") SearchRequest searchObj, Model model, BindingResult result) {
+        List<Order> orders = orts.findOrderbyId(searchObj.getOrderId());
+        model.addAttribute("orders", orders);
+
+        return "orderlist";
     }
 }
